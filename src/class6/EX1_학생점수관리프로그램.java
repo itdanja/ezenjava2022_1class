8168864d809b608b043b680c286892787b782aa2
501t <itdanja@kakao.com>
2022-08-05 20:40:54 +0900

    day06

--- src/class6/EX1_학생점수관리프로그램.java
+++ src/class6/EX1_학생점수관리프로그램.java
@@ -9,7 +9,7 @@ public class EX1_학생점수관리프로그램 { // class start
         int[] 학생점수목록 = null;  // 2.여러명 학생들의 점수를 저장하는 배열 선언
 
         while ( true ){ // 무한루프 [ 종료조건 : 4 입력했을때 종료된다 ]
-            System.out.println("1.학생수 2.점수입력 3.분석 4.종료");
+            System.out.println("1.학생수 2.점수입력 3.분석(높은순으로출력) 4.종료");
             System.out.print("안내) 선택 : "); int ch = scanner.nextInt();
             if( ch == 1 ){
                 System.out.print("안내) 학생수 : "); int count = scanner.nextInt();
@@ -22,7 +22,26 @@ public class EX1_학생점수관리프로그램 { // class start
                     학생점수목록[i] = scanner.nextInt();
                 }
             }
-            else if( ch == 3 ){ }
+            else if( ch == 3 ){
+                // 배열 정렬 [ 높은순으로 출력(내림차순) ]
+                for( int i = 0 ; i<학생점수목록.length ; i++ ){
+                    // i : 비교기준 [  i 는 0부터 배열의길이까지 1씩 증가한다.
+                    for( int j = i+1 ; j<학생점수목록.length ; j++ ){
+                        // j : 비교대상 [ j는 i+1 부터 배열의길이까지 1씩 증가한다.
+                        if( 학생점수목록[i] < 학생점수목록[j] ){
+                            // 비교대상이 더 크면 값 교환[스왑]
+                            int temp = 학생점수목록[i];
+                            학생점수목록[i] = 학생점수목록[j];
+                            학생점수목록[j] = temp;
+                        } // if end
+                    } // for end
+                } // for end
+                // 정렬 후 출력
+                for( int i = 0 ; i< 학생점수목록.length ; i++ ){
+                    System.out.println( (i+1)+"등 점수 : " + 학생점수목록[i] );
+                }
+
+            } // else if end
             else if( ch == 4 ){
                 System.out.println("안내) 프로그램을 종료 합니다.");
                 break;  /* 가장 가까운 반복문 탈출 */
@@ -31,3 +50,26 @@ public class EX1_학생점수관리프로그램 { // class start
         } // while end
     } // main end
 }// class end
+
+
+/*
+        90  95  90
+        i = 90 일때
+            j = 95  i < j
+            j = 90  i < j
+        i = 95 일때
+            j = 90  i < j
+
+
+ */
+
+
+
+
+
+
+
+
+
+
+
